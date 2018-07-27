95925e814652779dacf2452de9422d7232f09778
Paul Minniti <pminniti94@gmail.com>
2018-07-26 23:59:09 -0400

    Added like post route

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -9,6 +9,9 @@ const ValidatePostInput = require("../../Validation/post");
 // Load Post Model
 const Post = require("../../models/Post");
 
+// Load Profile Model
+const Profile = require("../../models/Profile");
+
 // @route GET api/posts/test
 // @desc Tests post route
 // @access Public
@@ -61,6 +64,63 @@ router.post(
     });
 
     newPost.save().then(post => res.json(post));
+  }
+);
+
+// @route DELETE api/posts/:id
+// @desc Delete Post
+// @access Private
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        // Check for post owner
+        if (post.user.toString() !== req.user.id) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+
+        //Delete
+        post
+          .remove()
+          .then(() =>
+            res
+              .json({ success: true })
+              .catch(err =>
+                res.status(404).json({ postnotfound: "No post found" })
+              )
+          );
+      });
+    });
+  }
+);
+
+// @route Post api/posts/like/:id
+// @desc Like Post
+// @access Private
+router.post(
+  "/like/:id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .lenght > 0
+        ) {
+          return res
+            .status(400)
+            .json({ alreadyliked: "User already liked this post" });
+        }
+
+        // Add user id to likes array
+        post.likes.unshift({ user: req.user.id });
+
+        // Save like to database
+        post.save().then(post => res.json(post));
+      });
+    });
   }
 );
 
